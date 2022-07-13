3fcc748442e374f7c3455adb9606e1c2648b2f07
Paul Walker <paul@pwjw.com>
2022-07-13 14:25:54 -0400

    Builds a gui-less synth

---
+++ src/clap-saw-demo-editor.cpp
@@ -0,0 +1,146 @@
+//
+// Created by Paul Walker on 6/10/22.
+//
+
+#include "clap-saw-demo-editor.h"
+#include "clap-saw-demo.h"
+
+
+namespace sst::clap_saw_demo
+{
+/*
+ * Part one of this implementation is the plugin adapters which allow a GUI to attach.
+ * These are the ClapSawDemo methods. First up: Which windowing API do we support.
+ * Pretty obviously, mac supports cocoa, windows supports win32, and linux supports
+ * X11.
+ */
+bool ClapSawDemo::guiIsApiSupported(const char *api, bool isFloating) noexcept
+{
+    if (isFloating)
+        return false;
+#if IS_MAC
+    if (strcmp(api, CLAP_WINDOW_API_COCOA) == 0)
+        return true;
+#endif
+
+#if IS_WIN
+    if (strcmp(api, CLAP_WINDOW_API_WIN32) == 0)
+        return true;
+#endif
+
+#if IS_LINUX
+    if (strcmp(api, CLAP_WINDOW_API_X11) == 0)
+        return true;
+#endif
+
+    return false;
+}
+
+/*
+ * GUICreate gets called when the host requests the plugin create its editor with
+ * a given API. We ignore the API and isFloating here, because we handled them
+ * above and assume our host follows the protocol that it only calls us with
+ * values which are supported.
+ *
+ * The important thing from a VSTGUI perspective here is that we have to initialize
+ * the VSTGUI static data structures. On Mac and Windows, this is an easy call and
+ * we can use the VSTGUI::finally mechanism to clean up. On Linux there is a more
+ * complicated global event loop to merge which, thanks to the way VSTGUI structures
+ * their event loops, is a touch more awkward. As such the linux code is all in a different
+ * cpp file for individual documentation (Please see the README for any linux disclaimers
+ * and most recent status).
+ */
+bool ClapSawDemo::guiCreate(const char *api, bool isFloating) noexcept
+{
+    _DBGMARK;
+
+    return false;
+}
+
+/*
+ * guiDestroy destroys the editor object and returns it to the
+ * nullptr sentinel, to stop ::process sending events to the ui.
+ */
+void ClapSawDemo::guiDestroy() noexcept
+{
+    if (editor)
+        delete editor;
+    editor = nullptr;
+}
+
+/*
+ * guiSetParent is the core API for a clap HOST which has a window to
+ * reparent the editor to that host managed window. It sends a
+ * `const clap_window *window` data structure which contains a union of
+ * platform specific window pointers.
+ *
+ * VSTGUI handles reparenting through `VSTGUI::CFrame::open` which consumes
+ * a pointer to a native window. This makes adapting easy. Our editor object
+ * owns a `CFrame` as its base window, and setParent opens it with the new
+ * parent platform specific item handed to it.
+ */
+bool ClapSawDemo::guiSetParent(const clap_window *window) noexcept
+{
+    if (dataCopyForUI.isProcessing)
+    {
+        // and ask the engine to refresh from the processing thread
+        refreshUIValues = true;
+    }
+    else
+    {
+        // Pull the parameters on the main thread
+        for (const auto &[k, v] : paramToValue)
+        {
+            auto r = ToUI();
+            r.type = ToUI::PARAM_VALUE;
+            r.id = k;
+            r.value = *v;
+            toUiQ.try_enqueue(r);
+        }
+    }
+    // And we are done!
+    return false;
+}
+
+/*
+ * guiSetScale is the core API that allows the Host to set the absolute GUI
+ * scaling factor, and override any OS info. This is important to allow the UI
+ * to correctly reflect what has been specified by the Host and not have to
+ * work out the users intentions through some sort of magic.
+ *
+ * Obviously, the value will depend on how the host chooses to implement it.
+ * The value is normalised, with 1.0 representing 100% scaling.
+ */
+bool ClapSawDemo::guiSetScale(double scale) noexcept
+{
+    _DBGCOUT << _D(scale) << std::endl;
+    return false;
+}
+
+/*
+ * Sizing is described in the gui extension, but this implementation
+ * means that if the host drags to resize, we accept its size and resize our frame
+ */
+bool ClapSawDemo::guiSetSize(uint32_t width, uint32_t height) noexcept
+{
+    _DBGCOUT << _D(width) << _D(height) << std::endl;
+    return false;
+}
+
+/*
+ * Returns the size of the UI window, presumable so a host can better layout plugin UIs
+ * if grouped together.
+ */
+bool ClapSawDemo::guiGetSize(uint32_t *width, uint32_t *height) noexcept
+{
+    return false;
+}
+
+
+bool ClapSawDemo::guiAdjustSize(uint32_t *width, uint32_t *height) noexcept
+{
+    return false;
+}
+
+
+} // namespace sst::clap_saw_demo
---
+++ src/clap-saw-demo-editor.h
@@ -0,0 +1,16 @@
+//
+// Created by Paul Walker on 6/10/22.
+//
+
+#ifndef CLAP_SAW_DEMO_EDITOR_H
+#define CLAP_SAW_DEMO_EDITOR_H
+#include "clap-saw-demo.h"
+
+namespace sst::clap_saw_demo
+{
+struct ClapSawDemoEditor
+{
+
+};
+} // namespace sst::clap_saw_demo
+#endif
